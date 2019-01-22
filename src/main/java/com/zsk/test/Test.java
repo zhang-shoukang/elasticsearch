5a60616e6a947b7489fa1f54d53f788c23412ded
zhang5houkang@163.com <zhang5houkang@163.com>
2019-01-22 20:56:54 +0800

    init project

---
+++ src/main/java/com/zsk/test/Test.java
@@ -0,0 +1,13 @@
+package com.zsk.test;
+
+/**
+ * @program: elasticsearch
+ * @description:
+ * @author: zhangshk
+ * @create: 2019-01-22 20:53
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("ff");
+    }
+}
